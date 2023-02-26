1e675a349decb3ff52141f47fda524f506952f23
slaveeks <chernyshev2001@icloud.com>
2023-02-26 18:35:54 +0300

    Added initial functionality

---
+++ editor.tool.ts
@@ -0,0 +1,11 @@
+export default function config(){
+
+    return {
+        setup: {
+            packageManager: 'yarn',
+            core: {
+                version: 'latest'
+            }
+        }
+    }
+}
