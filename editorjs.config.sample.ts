bbccdbafb65eb17365f008e2918ebdd0a88366f2
slaveeks <chernyshev2001@icloud.com>
2023-04-15 20:09:11 +0300

    Fixed code style in editorjs.config.sample.ts, added it to lint command

--- editorjs.config.sample.ts
+++ editorjs.config.sample.ts
@@ -1,27 +1,31 @@
-export default function config() {
-
-    return {
-        setup: {
-            packageManager: 'yarn',
-            core: {
-                version: '2.26.5'
-            },
-            tools: [
-                '@editorjs/raw',
-                {
-                    name: '@editorjs/table',
-                    version: '~2.2.0'
-                },
-                '@editorjs/code',
-                {
-                    name: 'header',
-                    path: './header/dist/bundle.js'
-                }
-            ]
+/**
+ * Create editor.js dev-tools config
+ *
+ * @returns {unknown} - editor.js dev-tools config
+ */
+export default function config(): unknown {
+  return {
+    setup: {
+      packageManager: 'yarn',
+      core: {
+        version: '2.26.5',
+      },
+      tools: [
+        '@editorjs/raw',
+        {
+          name: '@editorjs/table',
+          version: '~2.2.0',
         },
-        editorConfig: {
-            inlineToolbar: false,
-        }
-    }
-};
+        '@editorjs/code',
+        {
+          name: 'header',
+          path: './header/dist/bundle.js',
+        },
+      ],
+    },
+    editorConfig: {
+      inlineToolbar: false,
+    },
+  };
+}
 
